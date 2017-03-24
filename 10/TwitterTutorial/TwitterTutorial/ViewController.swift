8b01b3012fc19ec0e47b95f2e860545e5803b5c8
Andy Ron <randy.njfu@gmail.com>
2017-03-24 14:55:56 +0800

    TwitterTutorial
    Social framework

--- 10/TwitterTutorial/TwitterTutorial/ViewController.swift
+++ 10/TwitterTutorial/TwitterTutorial/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import Social
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var pickerController: UIImagePickerController  = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func chooseImagePressed(_ sender: UIButton) {
+        
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        present(pickerController, animated: true, completion: nil)
     }
 
 
+    @IBAction func tweetButtonPressed(_ sender: UIButton) {
+        
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+            
+            if let tweetSheet = SLComposeViewController(forServiceType: SLServiceTypeTwitter) {
+                
+                tweetSheet.setInitialText("看着漂亮的图片了")
+                tweetSheet.add(imageView.image)
+                
+                present(tweetSheet, animated: true, completion: nil)
+            }
+        } else {
+            print("error")
+        }
+    }
+    //MARK: - UIImagePickerControllerDelegate
+    // 获取图片后调用
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
 
