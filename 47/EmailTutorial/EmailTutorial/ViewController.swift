1dc13bbe50e4e2536cafb7213f997f7b1cb7588b
Andy Ron <randy.njfu@gmail.com>
2017-04-10 17:01:29 +0800

    EmailTutorial

--- 47/EmailTutorial/EmailTutorial/ViewController.swift
+++ 47/EmailTutorial/EmailTutorial/ViewController.swift
@@ -18,11 +18,42 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, UIT
         super.viewDidLoad()
 
         subject.delegate = self
+        body.delegate = self
         
     }
 
 
     @IBAction func sendEmail(_ sender: Any) {
+        let picker = MFMailComposeViewController()
+        picker.mailComposeDelegate = self
+        picker.setSubject(subject.text!)
+        picker.setMessageBody(body.text, isHTML: true)
+        
+        present(picker, animated: true, completion: nil)
+    }
+
+    //MARK: - MFMailComposeViewControllerDelegate
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: - UITextViewDelegate
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        body.text = textView.text
+        
+        if text == "\n" {
+            textView.resignFirstResponder()
+            
+            return false
+        }
+        
+        return true
     }
 
 }
