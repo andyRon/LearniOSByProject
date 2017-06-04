79df75b2c1a59e9612a7f7e9a7ded092a4c6eff0
Andy Ron <randy.njfu@gmail.com>
2017-06-04 10:30:04 +0800

    RecordPro

--- 65 Intermediate iOS 10 Programming with Swift/RecordPro/RecordPro/RecordProController.swift
+++ 65 Intermediate iOS 10 Programming with Swift/RecordPro/RecordPro/RecordProController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class RecordProController: UIViewController, AVAudioRecorderDelegate{
+class RecordProController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate{
 
     @IBOutlet private var stopButton: UIButton!
     @IBOutlet private var playButton: UIButton!
@@ -18,6 +18,9 @@ class RecordProController: UIViewController, AVAudioRecorderDelegate{
     
     var audioRecorder: AVAudioRecorder?
     var audioPlayer: AVAudioPlayer?
+    
+    private var timer: Timer?
+    private var elapsedTimeInSecond: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,15 +71,113 @@ class RecordProController: UIViewController, AVAudioRecorderDelegate{
     // MARK: - Action methods
     
     @IBAction func stop(sender: UIButton) {
+        recordButton.setImage(UIImage(named: "Record"), for: .normal)
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+        playButton.isEnabled = true
         
+        audioRecorder?.stop()
+        
+        resetTimer()
+        
+        let audioSeesion = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSeesion.setActive(false)
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func play(sender: UIButton) {
-        
+        if let recorder = audioRecorder {
+            if !recorder.isRecording {
+                audioPlayer = try? AVAudioPlayer(contentsOf: recorder.url)
+                audioPlayer?.delegate = self
+                audioPlayer?.play()
+                
+                recorder.pause()
+            }
+        }
     }
 
     @IBAction func record(sender: UIButton) {
-         
+        if let player = audioPlayer {
+            if player.isPlaying {
+                player.stop()
+            }
+        }
+        
+        if let recorder = audioRecorder {
+            if !recorder.isRecording {
+                let audioSession = AVAudioSession.sharedInstance()
+                
+                do {
+                    try audioSession.setActive(true)
+                    
+                    recorder.record()
+                    
+                    recordButton.setImage(UIImage(named: "Pause"), for: .normal)
+                } catch {
+                    print(error)
+                }
+            } else {
+                recorder.pause()
+                
+                recordButton.setImage(UIImage(named: "Record"), for: .normal)
+            }
+            
+            recorder.record()
+            startTimer()
+        }
+        
+        stopButton.isEnabled = true
+        playButton.isEnabled = false
+        
+        
     }
-
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            let alertMessage = UIAlertController(title: "录音完成", message: "成功记录一个音频", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        playButton.isSelected = false
+        
+        let alertMessage = UIAlertController(title: "Finsh Playing", message: "Finish playing the recording", preferredStyle: .alert)
+        alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertMessage, animated: true, completion: nil)
+    }
+    
+    // MARK: 实现Timer
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: {
+            (timer) in
+            self.elapsedTimeInSecond += 1
+            self.updateTimeLabel()
+            
+        
+        })
+    }
+    
+    func pauseTimer() {
+        timer?.invalidate()
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+        elapsedTimeInSecond = 0
+        updateTimeLabel()
+    }
+    func updateTimeLabel() {
+        let seconds = elapsedTimeInSecond % 60
+        let minutes = (elapsedTimeInSecond / 60) % 60
+        print(minutes)
+        timeLabel.text = String(format: "%02d:%02d", minutes, seconds)
+    }
+    
 }
