bdfc62852725f85ccbb44e1a1225fb97d22f9dbb
Andy Ron <randy.njfu@gmail.com>
2017-09-18 23:35:17 +0800

    mix

---
+++ 92/UIPasteboard/UIPasteboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIPasteboard
+//
+//  Created by andyron on 2017/9/18.
+//  Copyright © 2017年 andyron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 92/UIPasteboard/UIPasteboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIPasteboard
+//
+//  Created by andyron on 2017/9/18.
+//  Copyright © 2017年 andyron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
